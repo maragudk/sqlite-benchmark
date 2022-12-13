086347c4f90c2ae9e228a84be4c485917a601f24
Markus Wüstenberg <markus@maragu.dk>
2022-12-13 19:51:24 +0100

    Separate SQLite and Postgres benchmarks (#7)

--- sqlite_test.go
+++ sqlite_test.go
@@ -9,7 +9,6 @@ import (
 	"strconv"
 	"testing"
 
-	_ "github.com/jackc/pgx/v4/stdlib"
 	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/maragudk/sqlite-benchmark"
@@ -105,27 +104,6 @@ func BenchmarkDB_ReadPostAndMaybeWriteComment(b *testing.B) {
 	}
 }
 
-func BenchmarkDB_ReadPostAndMaybeWriteCommentPostgres(b *testing.B) {
-	db := setupPostgres(b, false)
-
-	b.ResetTimer()
-
-	for _, commentRate := range []float64{0.01, 0.1, 1} {
-		b.Run(fmt.Sprintf("comment rate %v", commentRate), func(b *testing.B) {
-			b.RunParallel(func(pb *testing.PB) {
-				for pb.Next() {
-					_, _, err := db.ReadPost(1)
-					noErr(b, err)
-					if rand.Float64() < commentRate {
-						err = db.WriteComment(1, "Love it!", "Great post. :D")
-						noErr(b, err)
-					}
-				}
-			})
-		})
-	}
-}
-
 //go:embed sqlite.sql
 var sqliteSchema string
 
@@ -137,31 +115,6 @@ func setupSQLite(tb testing.TB, withMutex bool) *sqlite.DB {
 
 	_, err = db.Exec(sqliteSchema)
 	noErr(tb, err)
-
-	newDB := sqlite.NewDB(db, withMutex)
-
-	err = newDB.WritePost("First post!", loremIpsum)
-	noErr(tb, err)
-
-	return newDB
-}
-
-//go:embed postgres.sql
-var postgresSchema string
-
-func setupPostgres(tb testing.TB, withMutex bool) *sqlite.DB {
-	tb.Helper()
-
-	db, err := sql.Open("pgx", "postgresql://test:123@localhost:5432/benchmark?sslmode=disable")
-	noErr(tb, err)
-
-	_, err = db.Exec(postgresSchema)
-	noErr(tb, err)
-
-	tb.Cleanup(func() {
-		_, err := db.Exec(`drop table comments; drop table posts;`)
-		noErr(tb, err)
-	})
 
 	newDB := sqlite.NewDB(db, withMutex)
 
